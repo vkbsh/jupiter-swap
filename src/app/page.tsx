8d54f8ed85ade28d590b842771c779dcf89f26cd
Vitaly Kibish <vitalykibish@MBP-van-Vitaly.arnhem.chello.nl>
2024-05-29 12:14:35 +0200

    add meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
+import type { Metadata } from 'next';
+
 import SwapScreen from 'components/screen/Swap';
+
+export const metadata: Metadata = {
+	title: 'Jupiter Swap',
+	description: 'Swap tokens with the Jupiter',
+};
 
 export default async function Home() {
 	return <SwapScreen />;
